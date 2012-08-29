5a43521543949a5a56df77e8b301cd27214a0071
Tom Wardill <tom@howrandom.net>
2012-08-29 17:10:33 +0100

    Cache for a time

--- src/byebyepypi/Handlers/ListHandler.py
+++ src/byebyepypi/Handlers/ListHandler.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import tornado.web
 import requests
 
@@ -5,11 +7,21 @@ from byebyepypi import settings
 
 class ListHandler(tornado.web.RequestHandler):
     
+    response_cache = {}
+    
     def get(self, egg):
         print 'EggHandler: ' + egg
+        
+        if self.response_cache.has_key(egg) and datetime.now() < (self.response_cache[egg]['time'] + settings.CACHE_TIME):
+                print 'Returning cache for: ' + egg
+                self.write(self.response_cache[egg]['response'])
+                return
+            
         
         r = requests.get(settings.PYPI_URL + egg)
         if (r.status_code > 200):
             raise tornado.web.HTTPError(r.status_code)
+        
+        self.response_cache[egg] = {'response': r.text, 'time': datetime.now()}
         
         self.write(r.text)
--- src/byebyepypi/settings.py
+++ src/byebyepypi/settings.py
@@ -1,1 +1,4 @@
+from datetime import timedelta
+
 PYPI_URL = 'http://pypi.python.org/simple/'
+CACHE_TIME = timedelta(minutes = 10)
