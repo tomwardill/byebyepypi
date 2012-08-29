49a815044ac164d008f196e721fde0f27e2131ff
Tom Wardill <tom@howrandom.net>
2012-08-29 21:56:16 +0100

    remove unnecessary logging

--- src/byebyepypi/handlers/package_handler.py
+++ src/byebyepypi/handlers/package_handler.py
@@ -10,12 +10,8 @@ class PackageHandler(tornado.web.RequestHandler):
     """
     
     def get(self, category, package_name, package_file):
-        logging.info(category)
-        logging.info(package_name)
-        logging.info(package_file)
         
         f = requests.get(settings.PYPI_URL + self.request.uri)
-        logging.info(len(f.content))
         self.set_header('Content-Disposition', 'attachment; filename=' + package_file)
         self.set_header('Content-Type', 'application/x-tar')
         self.write(f.content)
