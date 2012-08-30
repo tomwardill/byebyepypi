19cd164cd3f7c09f3c913a4a23d14bbfef17fb34
Tom Wardill <tom@howrandom.net>
2012-08-30 11:35:36 +0100

    Cache downloaded files

--- src/byebyepypi/handlers/package_handler.py
+++ src/byebyepypi/handlers/package_handler.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import tornado.web
 import requests
@@ -11,7 +12,24 @@ class PackageHandler(tornado.web.RequestHandler):
     
     def get(self, category, package_name, package_file):
         
+        if not os.path.exists(settings.FILE_DIR):
+            os.mkdir(settings.FILE_DIR)
+            
+        if os.path.exists(os.path.join(settings.FILE_DIR, package_file)):
+            logging.info('Cached file found')
+            cache = open(os.path.join(settings.FILE_DIR, package_file), 'rb')
+            self.set_header('Content-Disposition', 'attachment; filename=' + package_file)
+            self.set_header('Content-Type', 'application/x-tar')            
+            self.write(cache.read())
+            return
+        
         f = requests.get(settings.PYPI_URL + self.request.uri)
         self.set_header('Content-Disposition', 'attachment; filename=' + package_file)
         self.set_header('Content-Type', 'application/x-tar')
+        
+        # save the file to cache
+        cache = open(os.path.join(settings.FILE_DIR, package_file), 'wb')
+        cache.write(f.content)
+        cache.close()
+        
         self.write(f.content)
--- src/byebyepypi/settings.py
+++ src/byebyepypi/settings.py
@@ -2,5 +2,6 @@ from datetime import timedelta
 
 PYPI_INDEX_URL = 'http://pypi.python.org/simple/'
 PYPI_URL = 'http://pypi.python.org'
+FILE_DIR = 'cache'
 
 CACHE_TIME = timedelta(minutes = 10)
